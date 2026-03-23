0ec0d4449f68ff134d108ba4a46dcc8027623220
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-03-23 16:24:37 -0500

    set up testing with Vitest and Playwright

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,9 @@
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="flex items-center">
+        <header className="flex items-center justify-between">
             <Image 
                 src="/logo.png" 
                 alt={"Back to the Shelf Again logo: five upright books interspersed with foliage."}
@@ -11,6 +12,11 @@ export default function Header() {
                 className="w-20"
             />
 
+            <h1 className="">
+                Back to the Shelf Again
+            </h1>
+
+            <Bars3Icon className="size-8" />
         </header>
     );
 }
---
+++ vitest.setup.ts
@@ -0,0 +1,3 @@
+import "@testing-library/jest-dom";
+// This gives you matchers like .toBeInTheDocument(), .toBeVisible(), .toHaveTextContent(), etc.
+// It runs before every test file automatically (via setupFiles in vitest.config.ts)
