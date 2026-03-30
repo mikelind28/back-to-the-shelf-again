37aa6359e4d12a3c43376ff22cb992043175d7b8
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-03-30 00:37:08 -0500

    forgot to run prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./e2e",
-  timeout: 30_000,
+  timeout: 15_000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
