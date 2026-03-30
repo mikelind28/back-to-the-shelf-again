fbcb3822e0d5c2c1b4e672ea5f8bf2ac59933c83
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-03-30 01:16:34 -0500

    run prettier

--- collections/Media.ts
+++ collections/Media.ts
@@ -3,7 +3,7 @@ import type { CollectionConfig } from "payload";
 export const Media: CollectionConfig = {
   slug: "media",
   upload: {
-    staticDir: '../public/media',
+    staticDir: "../public/media",
   },
   access: {
     read: () => true,
