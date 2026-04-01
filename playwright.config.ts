cc4ea6f24e0c0255b2b8063ca1166d6dd8831e7c
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-04-01 13:00:45 -0500

    make some playwright yml changes for quicker ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,13 +20,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? [["list"], ["html"]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.PLAYWRIGHT_BASE_URL ?? "http://localhost:3000",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -39,44 +39,36 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
       testMatch: ["tests/all/**", "tests/desktop/**"],
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-      testMatch: ["tests/all/**", "tests/desktop/**"],
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-      testMatch: ["tests/all/**", "tests/desktop/**"],
-    },
-    /* Test against mobile viewports. */
+    ...(process.env.CI_FULL
+      ? [
+          {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+            testMatch: ["tests/all/**", "tests/desktop/**"],
+          },
+        ]
+      : []),
     {
       name: "Mobile Chrome",
       use: { ...devices["Pixel 5"] },
       testMatch: ["tests/all/**", "tests/mobile/**"],
     },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone SE"] },
-      testMatch: ["tests/all/**", "tests/mobile/**"],
-    },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    ...(process.env.CI_FULL
+      ? [
+          {
+            name: "Mobile Safari",
+            use: { ...devices["iPhone SE"] },
+            testMatch: ["tests/all/**", "tests/mobile/**"],
+          },
+        ]
+      : []),
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run build && npm run start",
+    command: "npm run dev",
     url: "http://localhost:3000",
-    timeout: 120_000,
+    timeout: 180_000,
     reuseExistingServer: !process.env.CI,
   },
 });
